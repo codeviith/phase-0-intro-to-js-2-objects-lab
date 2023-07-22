ca641da86fbaa40a9fe52696567f39b60adacc5f
codeviith <codeviith@Cinya>
2023-07-22 01:21:10 -0400

    JS Lab 8

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution in this file!
+const employee = {
+    name: "Stan",
+    streetAddress: "8138 Lassi Ave.",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let clonedEmployee = {...employee};
+
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    
+    return employee;
+}
+
+
+
+
+
+
+
+
+
+
+
+
